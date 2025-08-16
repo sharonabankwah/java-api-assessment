f4ffe96484afb7b014047f03126c47d4537f7072
Sharon <sharonabankwah@gmail.com>
2025-08-16 17:44:39 +0000

    Initial commit: Added Moodcha Spring Boot project

---
+++ moodcha/src/main/java/com/moodcha/moodcha/MoodchaApplication.java
@@ -0,0 +1,13 @@
+package com.moodcha.moodcha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodchaApplication.class, args);
+	}
+
+}
---
+++ moodcha/src/test/java/com/moodcha/moodcha/MoodchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moodcha.moodcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
