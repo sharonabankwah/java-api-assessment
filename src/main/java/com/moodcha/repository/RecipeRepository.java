e7eadd322ccdb26f743cdb8b00b09cfccb5c7631
Sharon <sharonabankwah@gmail.com>
2025-09-01 20:03:04 +0000

    feat: create Recipe repository with query methods

--- src/main/java/com/moodcha/repository/RecipeRepository.java
+++ src/main/java/com/moodcha/repository/RecipeRepository.java
@@ -3,9 +3,19 @@ package com.moodcha.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.moodcha.model.Recipe;
 import com.moodcha.model.enums.Mood;
+import com.moodcha.model.enums.Flavour;
+import com.moodcha.model.enums.Milk;
+import java.util.List;
+import java.util.UUID;
 
 public interface RecipeRepository extends JpaRepository<Recipe, UUID> {
 
+List<Recipe> findByMood(Mood mood);
 
+List<Recipe> findByFlavour(Flavour flavour);
+
+List<Recipe> findByMilk(Milk milk);
+
+List<Recipe> findByMoodAndFlavourAndMilk(Mood mood, Flavour flavour, Milk milk);
 
 }
