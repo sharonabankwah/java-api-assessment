903259226f294ff9e193d9753f6efd0e527de3e3
Sharon <sharonabankwah@gmail.com>
2025-09-01 00:04:46 +0000

    feat: extend JPA repository

--- src/main/java/com/moodcha/repository/RecipeRepository.java
+++ src/main/java/com/moodcha/repository/RecipeRepository.java
@@ -1,5 +1,11 @@
 package com.moodcha.repository;
 
-public class RecipeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.moodcha.model.Recipe;
+import com.moodcha.model.enums.Mood;
+
+public interface RecipeRepository extends JpaRepository<Recipe, UUID> {
+
+
 
 }
