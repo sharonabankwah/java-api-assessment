d61ddaa37f2f8722cbf3a908a9e7c7c547445a78
Sharon <sharonabankwah@gmail.com>
2025-09-04 16:35:12 +0000

    feat: add queries to water repository

--- src/main/java/com/moodcha/repository/WaterBasedRepository.java
+++ src/main/java/com/moodcha/repository/WaterBasedRepository.java
@@ -1,8 +1,20 @@
 package com.moodcha.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.moodcha.model.WaterBasedRecipe;
+import com.moodcha.model.enums.Flavour;
+import com.moodcha.model.enums.Mood;
+import com.moodcha.model.enums.WaterType;
 import java.util.UUID;
+import java.util.List;
 
 public interface WaterBasedRepository extends JpaRepository<WaterBasedRepository, UUID> {
+
+  List<WaterBasedRecipe> findByMood(Mood mood);
+
+  List<WaterBasedRecipe> findByFlavour(Flavour flavour);
+
+  List<WaterBasedRecipe> findByWaterType(WaterType water);
 
 }
