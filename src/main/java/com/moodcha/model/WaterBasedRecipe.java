3f8382a9e483b55ee2a541911e2108395b5a5f8d
Sharon <sharonabankwah@gmail.com>
2025-09-04 15:36:41 +0000

    feat: create child class that implements water based recipes

---
+++ src/main/java/com/moodcha/model/WaterBasedRecipe.java
@@ -0,0 +1,40 @@
+package com.moodcha.model;
+
+import com.moodcha.model.enums.Flavour;
+import com.moodcha.model.enums.Mood;
+import com.moodcha.model.enums.Temperature;
+import com.moodcha.model.enums.WaterType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "water_recipes")
+public class WaterBasedRecipe extends BaseRecipe {
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  private WaterType water;
+
+  public WaterBasedRecipe(Mood mood, Flavour flavour, Temperature temperature, String supplements, 
+                         String allergies, WaterType water) {
+      super(mood, flavour, temperature, supplements, allergies);
+      this.water = water;
+  }
+
+  public WaterBasedRecipe() {
+    // no-arg constructor
+    super();
+  }
+
+  public WaterType getWater() {
+    return water;
+  }
+
+  public void setWater(WaterType water) {
+    this.water = water;
+  }
+
+}
