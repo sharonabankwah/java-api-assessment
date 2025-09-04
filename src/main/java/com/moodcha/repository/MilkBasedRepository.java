95fcd46713cd435e70fa5d333c6b5e2768c61686
Sharon <sharonabankwah@gmail.com>
2025-09-04 16:35:44 +0000

    feat: amend queries in milk repository

--- src/main/java/com/moodcha/repository/MilkBasedRepository.java
+++ src/main/java/com/moodcha/repository/MilkBasedRepository.java
@@ -10,26 +10,30 @@ import com.moodcha.model.enums.MilkType;
 import java.util.List;
 import java.util.UUID;
 import java.util.Optional;
+import com.moodcha.model.enums.Temperature;
+
 
 public interface MilkBasedRepository extends JpaRepository<MilkBasedRecipe, UUID> {
 
-List<MilkBasedRecipe> findByMood(Mood mood);
+  List<MilkBasedRecipe> findByMood(Mood mood);
 
-List<MilkBasedRecipe> findByFlavour(Flavour flavour);
+  List<MilkBasedRecipe> findByFlavour(Flavour flavour);
 
-List<MilkBasedRecipe> findByMilk(MilkType milk);
+  List<MilkBasedRecipe> findByMilkType(MilkType milk);
 
-List<MilkBasedRecipe> findByMoodAndFlavourAndMilk(Mood mood, Flavour flavour, MilkType milk);
+  List<MilkBasedRecipe> findByTemperature(Temperature temperature);
 
-@Query("SELECT r FROM Recipe WHERE r.mood = :mood AND r.supplements IS NOT NULL")
-List<MilkBasedRecipe> findByMoodWithSupplements(@Param("mood")Mood mood);
+  List<MilkBasedRecipe> findByMoodAndFlavourAndMilkType(Mood mood, Flavour flavour, MilkType milk);
 
-@Query("SELECT r FROM Recipe WHERE :allergy NOT MEMBER OF r.allergies")
-List<MilkBasedRecipe> findRecipesWithoutAllergy(@Param("allergy")String allergy);
+  @Query("SELECT r FROM MilkBasedRecipe WHERE r.mood = :mood AND r.supplements IS NOT NULL")
+  List<MilkBasedRecipe> findByMoodWithSupplements(@Param("mood")Mood mood);
 
-@Query(value = "SELECT * FROM recipes" +
-               "WHERE mood IS NOT NULL AND flavour IS NOT NULL AND milk IS NOT NULL" +
-               "ORDER BY RAND() LIMIT 1", nativeQuery = true)
-Optional<MilkBasedRecipe> findRandomRecipe();
+  @Query("SELECT r FROM MilkBasedRecipe WHERE :allergy NOT MEMBER OF r.allergies")
+  List<MilkBasedRecipe> findRecipesWithoutAllergy(@Param("allergy")String allergy);
+
+  @Query(value = "SELECT * FROM MilkBasedRecipe" +
+                 "WHERE mood IS NOT NULL AND flavour IS NOT NULL AND milk IS NOT NULL" +
+                 "ORDER BY RAND() LIMIT 1", nativeQuery = true)
+  Optional<MilkBasedRecipe> findRandomRecipe();
 
 }
