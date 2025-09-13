44a33f1a8c0e49513d1762f786b84786cd03f7be
Sharon <sharonabankwah@gmail.com>
2025-09-13 21:04:37 +0000

    feat: add endpoints for additional query filters

--- src/main/java/com/moodcha/controller/RecipeController.java
+++ src/main/java/com/moodcha/controller/RecipeController.java
@@ -1,10 +1,18 @@
 package com.moodcha.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.moodcha.model.BaseRecipe;
+import com.moodcha.model.MilkBasedRecipe;
 import com.moodcha.service.RecipeService;
+import com.moodcha.model.enums.Mood;
+import com.moodcha.model.enums.Flavour;
+import com.moodcha.model.enums.MilkType;
+import com.moodcha.model.enums.Temperature;
+import com.moodcha.model.enums.SyrupType;
 import java.util.UUID;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -46,6 +54,36 @@ public class RecipeController {
   public ResponseEntity<Void> deleteRecipeById(@PathVariable UUID id) {
     recipeService.deleteRecipeById(id);
     return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping("/mood")
+  public ResponseEntity<List<BaseRecipe>> getRecipeByMood(@RequestParam("mood") Mood mood) {
+    return ResponseEntity.ok(recipeService.getAllMoodsRecipes(mood));
+  }
+
+  @GetMapping("/flavour")
+  public ResponseEntity<List<BaseRecipe>> getRecipeByFlavour(@RequestParam("flavour") Flavour flavour) {
+    return ResponseEntity.ok(recipeService.getAllFlavoursRecipes(flavour));
+  }
+
+  @GetMapping("/milk")
+  public ResponseEntity<List<MilkBasedRecipe>> getRecipeByMilk(@RequestParam("milk") MilkType milk) {
+    return ResponseEntity.ok(recipeService.getSpecificMilkRecipes(milk));
+  }
+
+  @GetMapping("/temperature")
+  public ResponseEntity<List<BaseRecipe>> getRecipeByTemperature(@RequestParam("temperature") Temperature temperature) {
+    return ResponseEntity.ok(recipeService.getRecipesByTemperature(temperature));
+  }
+
+  @GetMapping("/syrup")
+  public ResponseEntity<List<BaseRecipe>> getRecipeBySyrup(@RequestParam("syrup") SyrupType syrup) {
+    return ResponseEntity.ok(recipeService.getAllSyrupRecipes(syrup));
+  }
+
+  @GetMapping("/random")
+  public ResponseEntity<BaseRecipe> getRandomRecipe() {
+    return ResponseEntity.ok(recipeService.getRandomRecipe());
   }
 
 }
